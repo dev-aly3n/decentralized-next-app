ba676f52d5063e5034803e9550b3a3068a93a882
aly3n <aly3n.dev@gmail.com>
2022-03-17 08:05:12 +0330

    more clean up

--- src/components/Ether/index.tsx
+++ src/components/Ether/index.tsx
@@ -1,10 +1,19 @@
-const Ether:React.FC = () => {
+const Ether: React.FC = () => {
 
 
-    return (
-        <></>
-    );
-}
-
+    const etherClickHandler = async () => {
+        // const accounts = await window.ethereum
+    }
+  return (
+    <div className=" flex justify-center items-center h-96 w-96 bg-green-200 text-black">
+      <button
+        onClick={etherClickHandler}
+        className="p-2 bg-yellow-200 hover:bg-yellow-400 shadow-xl rounded-xl border-2 border-yellow-600 duration-500"
+      >
+        connect your wallet
+      </button>
+    </div>
+  );
+};
 
 export default Ether;
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -2,10 +2,8 @@ import Link from "next/link";
 
 import Head from "next/head";
 import { useRouter } from "next/router";
-import EachLink from "./EachLink";
 import { motion } from "framer-motion";
 // types
-import { Links } from "./EachLink";
 import { useCallback, useState } from "react";
 
 const Header: React.FC = () => {
@@ -17,48 +15,13 @@ const Header: React.FC = () => {
     [setNavHovered]
   );
 
-  const headerLinks: Links[] = [
-    {
-      ref: "/",
-      text: "Home",
-      hovered: false,
-    },
-    {
-      ref: "/profile",
-      text: "My Profile",
-      hovered: false,
-    },
-    {
-      ref: "/hiw",
-      text: "How It Works",
-      hovered: false,
-    },
-    {
-      ref: "/gallery",
-      text: "All NFTS",
-      hovered: false,
-    },
-  ];
-
   return (
     <>
       <div className="header-container">
         <h1>
           <Link href="/">Nepolia</Link>
         </h1>
-        <motion.ul
-        >
-          {headerLinks.map((headLink) => {
-            return (
-              <EachLink
-                key={headLink.ref}
-                headLink={headLink}
-                navHovered={navHovered}
-                navHovering={navHovering}
-              />
-            );
-          })}
-        </motion.ul>
+
       </div>
     </>
   );
