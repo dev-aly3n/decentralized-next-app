ddf8f0e563c595d367c975cee8cd30721e17abf2
aly3n <aly3n.dev@gmail.com>
2022-03-17 08:39:06 +0330

    connecting to metamask

--- src/components/Ether/index.tsx
+++ src/components/Ether/index.tsx
@@ -1,9 +1,18 @@
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider;
+  }
+}
+
 const Ether: React.FC = () => {
-
-
-    const etherClickHandler = async () => {
-        // const accounts = await window.ethereum
-    }
+  const etherClickHandler = async () => {
+    const accounts = await window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .catch((err) => console.log(err));
+    console.log(accounts);
+  };
   return (
     <div className=" flex justify-center items-center h-96 w-96 bg-green-200 text-black">
       <button
